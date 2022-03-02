e9847e2c6105d4ec6eef0bf7b265a837bd39a3e0
taesunpark <tovbskvb@daum.net>
2022-03-02 20:06:29 +0900

    [add] @PostConstruct, @PreDestroy 사용해서 빈 생성 과정 테스트

---
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,46 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient(){
+        System.out.println("생성자 호출, url"+ url);
+    }
+
+    public void setUrl(String url){
+        this.url = url;
+    }
+
+    // 서비스 시작 시 호출
+    public void connect(){
+        System.out.printf("connect"+ url);
+    }
+
+    public void call(String message){
+        System.out.printf("call: "+url+"message= "+ message);
+    }
+
+    public void disconnect(){
+        System.out.printf("close" + url);
+    }
+
+    @PostConstruct
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+    }
+}
